a10baf2df17d6a4ab11a0c6c31ec19091931dc91
kim leang <nonametotell4@gmail.com>
2023-06-07 17:13:34 +0700

    Add

---
+++ BlazorApp2/Class/ReportLayout.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Reporting.NETCore;
+using Newtonsoft.Json;
+using System.Data;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+public class ReportLayout
+{
+    public ReportLayout()
+    {
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+    }
+
+    public Task<PrintViewLayoutResponse> CallViewLayout(string Code, string docEntry/*, IWebAssemblyHostEnvironment _webHostEnvironment*/)
+    {
+        try
+        {
+            var reportSetup = GetParamaterLayout(Code).Result;
+            string rootpath = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "wwwroot");
+            var path = $"{rootpath}\\Report\\{reportSetup.Rows[0]["FILENAME"]}";
+            var type = getTypeExport(reportSetup.Rows[0]["EXPORTTYPE"].ToString()!);
+            var tes1 = reportSetup.Rows[0]["PROPERTIES"].ToString();
+            var reportBody = JsonConvert.DeserializeObject<List<ReportBodyResponse>>(reportSetup.Rows[0]["PROPERTIES"].ToString());
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            LocalReport lr = new LocalReport();
+            Stream reportDefinition = System.IO.File.OpenRead(path);
+            lr.LoadReportDefinition(reportDefinition);
+            foreach (var a in reportBody)
+            {
+                DataTable dt = GetValueFromStore(a.TypeOfParameter.ToString()!, docEntry).Result;
+                lr.DataSources.Add(new ReportDataSource(a.DataSetName.ToString()!, dt));
+            }
+            lr.Refresh();
+            var result = lr.Render(reportSetup.Rows[0]["EXPORTTYPE"].ToString()!);
+            return Task.FromResult(new PrintViewLayoutResponse
+            {
+                ErrorCode = 0,
+                ErrorMessage = "",
+                Data = result,
+                ApplicationType = type.Item2,
+                FileName = type.Item3
+            });
+        }
+        catch (Exception ex)
+        {
+            return Task.FromResult(new PrintViewLayoutResponse
+            {
+                ErrorCode = ex.GetHashCode(),
+                ErrorMessage = ex.Message
+            });
+        }
+    }
+    private Tuple<string, string, string> getTypeExport(string type)
+    {
+        if (type == "PDF")
+        {
+            return Tuple.Create("PDF", "application/pdf", "");
+        }
+        else if (type == "WORD")
+        {
+            return Tuple.Create("WORD", "application/msword", "word.doc");
+        }
+        else if (type == "EXCEL")
+        {
+            return Tuple.Create("EXCEL", "application/xlsx", "excel.xls");
+        }
+        return Tuple.Create("PDF", "application/pdf", "");
+    }
+    //private Task<DataTable> GetValueFromStore(string type, string docEntry)
+    //{
+    //    return Task.FromResult(new GetRecordByDataTable(
+    //                     GetRecordByDataTable.StoreType.Layout,
+    //                     type,
+    //                     docEntry,
+    //                     "",
+    //                     "",
+    //                     "",
+    //                     "").GetResultDataTable());
+    //}
+    //private Task<DataTable> GetParamaterLayout(string Code)
+    //{
+    //    return Task.FromResult(new GetRecordByDataTable(
+    //                     GetRecordByDataTable.StoreType.Config,
+    //                     Configure.LayoutPrintType!,
+    //                     Code,
+    //                     "",
+    //                     "",
+    //                     "",
+    //                     "").GetResultDataTable());
+    //}
+}
---
+++ ConvertAmountToTHB/Program.cs
@@ -0,0 +1,140 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var st = ThaiBaht("319321");
+Console.WriteLine(st.ToString());
+string a = "\u2649";
+
+Console.WriteLine(a);
+
+static string ThaiBaht(string txt)
+{
+    string bahtTxt, n, bahtTH = "";
+    double amount;
+    try { amount = Convert.ToDouble(txt); }
+    catch { amount = 0; }
+    bahtTxt = amount.ToString("####.00");
+    string[] num = { "ศูนย์", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า", "สิบ" };
+    string[] rank = { "", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน" };
+    string[] temp = bahtTxt.Split('.');
+    string intVal = temp[0];
+    string decVal = temp[1];
+    if (Convert.ToDouble(bahtTxt) == 0)
+        bahtTH = "ศูนย์บาทถ้วน";
+    else
+    {
+        for (int i = 0; i < intVal.Length; i++)
+        {
+            n = intVal.Substring(i, 1);
+            if (n != "0")
+            {
+                if ((i == (intVal.Length - 1)) && (n == "1"))
+                    bahtTH += "เอ็ด";
+                else if ((i == (intVal.Length - 2)) && (n == "2"))
+                    bahtTH += "ยี่";
+                else if ((i == (intVal.Length - 2)) && (n == "1"))
+                    bahtTH += "";
+                else
+                    bahtTH += num[Convert.ToInt32(n)];
+                bahtTH += rank[(intVal.Length - i) - 1];
+            }
+        }
+        bahtTH += "บาท";
+        if (decVal == "00")
+            bahtTH += "ถ้วน";
+        else
+        {
+            for (int i = 0; i < decVal.Length; i++)
+            {
+                n = decVal.Substring(i, 1);
+                if (n != "0")
+                {
+                    if ((i == decVal.Length - 1) && (n == "1"))
+                        bahtTH += "เอ็ด";
+                    else if ((i == (decVal.Length - 2)) && (n == "2"))
+                        bahtTH += "ยี่";
+                    else if ((i == (decVal.Length - 2)) && (n == "1"))
+                        bahtTH += "";
+                    else
+                        bahtTH += num[Convert.ToInt32(n)];
+                    bahtTH += rank[(decVal.Length - i) - 1];
+                }
+            }
+            bahtTH += "สตางค์";
+        }
+    }
+    return bahtTH;
+}
+
+/*
+ Public Shared Function ConvertToThaiBaht(ByVal txt As String) As String
+    Dim bahtTxt, n As String, bahtTH As String = ""
+    Dim amount As Double
+
+    Try
+        amount = Convert.ToDouble(txt)
+    Catch
+        amount = 0
+    End Try
+
+    bahtTxt = amount.ToString("####.00")
+    Dim num As String() = {"ศูนย์", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า", "สิบ"}
+    Dim rank As String() = {"", "สิบ", "ร้อย", "พัน", "หมื่น", "แสน", "ล้าน"}
+    Dim temp As String() = bahtTxt.Split("."c)
+    Dim intVal As String = temp(0)
+    Dim decVal As String = temp(1)
+
+    If Convert.ToDouble(bahtTxt) = 0 Then
+        bahtTH = "ศูนย์บาทถ้วน"
+    Else
+
+        For i As Integer = 0 To intVal.Length - 1
+            n = intVal.Substring(i, 1)
+
+            If n <> "0" Then
+
+                If (i = (intVal.Length - 1)) AndAlso (n = "1") Then
+                    bahtTH += "เอ็ด"
+                ElseIf (i = (intVal.Length - 2)) AndAlso (n = "2") Then
+                    bahtTH += "ยี่"
+                ElseIf (i = (intVal.Length - 2)) AndAlso (n = "1") Then
+                    bahtTH += ""
+                Else
+                    bahtTH += num(Convert.ToInt32(n))
+                End If
+
+                bahtTH += rank((intVal.Length - i) - 1)
+            End If
+        Next
+
+        bahtTH += "บาท"
+
+        If decVal = "00" Then
+            bahtTH += "ถ้วน"
+        Else
+
+            For i As Integer = 0 To decVal.Length - 1
+                n = decVal.Substring(i, 1)
+
+                If n <> "0" Then
+
+                    If (i = decVal.Length - 1) AndAlso (n = "1") Then
+                        bahtTH += "เอ็ด"
+                    ElseIf (i = (decVal.Length - 2)) AndAlso (n = "2") Then
+                        bahtTH += "ยี่"
+                    ElseIf (i = (decVal.Length - 2)) AndAlso (n = "1") Then
+                        bahtTH += ""
+                    Else
+                        bahtTH += num(Convert.ToInt32(n))
+                    End If
+
+                    bahtTH += rank((decVal.Length - i) - 1)
+                End If
+            Next
+
+            bahtTH += "สตางค์"
+        End If
+    End If
+
+    Return bahtTH
+End Function
+ */
